79085bb3a7d595f03449d9e251a1a6922f41db25
amulyagarimella <amu.garimella@gmail.com>
2022-12-13 21:28:50 -0500

    expand fixes

--- quiz/expand.js
+++ quiz/expand.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
     let show = document.querySelectorAll(".show");
     for (let i=0; i < show.length; i++) {
         show[i].addEventListener('click', function() {
+            show[i].parentElement.style.marginBottom = "5px";
             let hidden = document.querySelectorAll(".hidden." + show[i].classList[1]);
             let replace = document.querySelectorAll(".replace." + show[i].classList[1]);
             for (let j=0; j < hidden.length; j++)
             {
                 hidden[j].style.display = "block";
-                
             }
             let samelvl = document.querySelectorAll(".show." + show[i].classList[1]);
             for (let j=0; j < samelvl.length; j++) {
