afa618eac5180a28d7d01c6c8263f8b7eeb8d1cd
amulyagarimella <amu.garimella@gmail.com>
2022-12-13 21:25:52 -0500

    hider fixes

--- quiz/hider.js
+++ quiz/hider.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
             let hidden = document.querySelectorAll(".hidden." + show[i].classList[1]);
             for (let j=0; j < hidden.length; j++)
             {
-                hidden[j].style.display = "inline-block";
+                hidden[j].style.display = "block";
                 hidden[j].style.marginTop = "auto";
             }
             let samelvl = document.querySelectorAll(".show." + show[i].classList[1]);
